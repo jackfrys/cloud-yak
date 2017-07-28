ced7a8207cd2fd750e62165c5bf600a8d4a4d5da
Jack Frysinger <frysinger.j@husky.neu.edu>
2017-07-28 00:58:24 -0400

    Initial commit

---
+++ CloudYak/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CloudYak
+//
+//  Created by Jack Frysinger on 7/28/17.
+//  Copyright © 2017 Jack Frysinger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
