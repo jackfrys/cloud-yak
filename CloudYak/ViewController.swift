286d632447267ae3def55b741e6a76b5d22cc2c7
Jack Frysinger <frysinger.j@husky.neu.edu>
2017-11-11 18:45:11 -0500

    updated to swift 4

--- CloudYak/ViewController.swift
+++ CloudYak/ViewController.swift
@@ -8,11 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, YakModelDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    let model = YakModel()
+    
+    var yaks = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        model.delegate = self
+        model.refreshYaks()
+        
+        tableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,57 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func addYak(_ sender: Any) {
+        let alert = UIAlertController(title: "Enter your message", message: nil, preferredStyle: UIAlertControllerStyle.alert)
+        var text = UITextField()
+        alert.addTextField {(field) in text = field}
+        alert.addAction(UIAlertAction(title: "Post", style: UIAlertActionStyle.default) {a in self.model.postYak(message: text.text!)})
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func didRefreshYaks() {
+        yaks = model.recentResults
+        tableView.reloadData()
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return yaks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let content = yaks[indexPath.row]
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = content
+        
+        return cell
+    }
+    
+    func failedToRefreshYaks() {
+        showAlertWithText(title: "Could not refresh Yaks", message: "Check your network connection and try again")
+    }
+    
+    func userLocationNotAvailable() {
+        showAlertWithText(title: "Location unknown", message: "Check your location settings and try again")
+    }
+    
+    func didPostYak() {
+        showAlertWithText(title: "Yak Posted!", message: nil)
+        model.refreshYaks()
+    }
+    
+    func failedToPostYak() {
+        showAlertWithText(title: "Failed to post yak", message: "Check your network connection and try again")
+    }
+    
+    func showAlertWithText(title: String?, message: String?) {
+        let controller = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
+        controller.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default) {a in self.dismiss(animated: true, completion: nil)})
+        present(controller, animated: true, completion: nil)
+    }
+        
 }
 
---
+++ CloudYak/YakModel.swift
@@ -0,0 +1,81 @@
+//
+//  YakModel.swift
+//  CloudYak
+//
+//  Created by Jack Frysinger on 7/28/17.
+//  Copyright © 2017 Jack Frysinger. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+import CloudKit
+
+class YakModel : NSObject, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+    public var delegate: YakModelDelegate?
+    
+    let publicDb = CKContainer.default().publicCloudDatabase
+    
+    var recentResults = [String]()
+    
+    override init() {
+        super.init()
+        
+        locationManager.delegate = self;
+        
+        if CLLocationManager.authorizationStatus() == .notDetermined {
+            locationManager.requestWhenInUseAuthorization();
+        }
+        
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            locationManager.startUpdatingLocation();
+        } else {
+            delegate?.userLocationNotAvailable();
+        }
+    }
+    
+    public func refreshYaks() {
+        let query = CKQuery(recordType: "Yaks", predicate: NSPredicate(value: true))
+        publicDb.perform(query, inZoneWith: nil) {results, error in
+            if let r = results {
+                self.recentResults = r.map({$0.object(forKey: "text") as! String})
+                self.delegate?.didRefreshYaks()
+            } else {
+                self.delegate?.failedToRefreshYaks()
+            }
+        }
+    }
+    
+    public func postYak(message: String) {
+        if let l = locationManager.location {
+            let type = CKRecord(recordType: "Yaks")
+            type.setValue(message, forKey: "text")
+            type.setObject(l, forKey: "location")
+            self.publicDb.save(type) {record, error in
+                if let e = error {
+                    print(e)
+                    self.delegate?.failedToPostYak()
+                } else {
+                    self.delegate?.didPostYak()
+                }
+            }
+        } else {
+            delegate?.userLocationNotAvailable()
+        }
+    }
+    
+}
+
+protocol YakModelDelegate {
+    
+    func didRefreshYaks();
+    
+    func failedToRefreshYaks();
+    
+    func didPostYak();
+    
+    func failedToPostYak();
+    
+    func userLocationNotAvailable();
+}
